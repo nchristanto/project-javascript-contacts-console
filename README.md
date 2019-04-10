85b3d64b978e2422103893d1ed9a1578d721e1f5
nchristanto <christanto.nanda@gmail.com>
2019-04-10 22:15:16 +0700

    initialize index.js, index.html, README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Javascript Contacts Console
+
+Project Javascript to create contact console:
+- Show contacts using array
+- Call contacts using array
+- Splice array
+- Slice array
+- Unshift array
+- Sort contacts using array
+- Show contacts using object
+- Filter contacts using object
+- Add contacts  using object
---
+++ index.js
@@ -0,0 +1,137 @@
+// Array Data
+const names = ["Raisa", "Budiman", "Denny", "Agus"];
+
+const showNames = names => {
+  for (let index = 0; index < names.length; index++) {
+    const name = names[index];
+
+    console.log(`[${index + 1}] ${name}`);
+  }
+};
+
+showNames(names)
+console.log("");
+
+// Sort using FOR Loop # 1
+const item = ["Raisa", "Budiman", "Denny", "Agus"];
+
+const sortItem = item => {
+  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let newItem = []
+  for (let indexi = 0; indexi < alphabet.length; indexi++) {
+    for (let indexj = 0; indexj < item.length; indexj++) {
+      if (alphabet[indexi] === item[indexj][0]) {
+        newItem.push(item[indexj])
+      }
+    }
+  }
+  return newItem
+}
+console.log(sortItem(item))
+console.log("");
+
+// Raga sort # 2
+const sortNames = names => {
+  return names.sort((item, comparator) => {
+    if (item > comparator) return 1;
+    else if (item < comparator) return -1;
+    return 0;
+  });
+};
+console.log(sortNames(names));
+console.log("");
+
+ 
+// Array shift
+console.log(names.shift())
+console.log(names)
+console.log("")
+
+// Array splice 
+console.log(item.splice(2, 1, 'Joko'))
+console.log(item)
+console.log("")
+
+// Array slice 
+console.log(item.slice(1, 3))
+console.log("")
+
+// Object Data
+const myContacts = [
+  {
+    id: 1,
+    name: "Raisa",
+    age: 28,
+    phone: "+1 111 101010",
+    email: "raisa@.com",
+    organization: "Impact Byte",
+    favorite: true,
+    rating: 9,
+    tags: ["popular", "pretty"]
+  },
+  {
+    id: 2,
+    name: "John Doe",
+    age: 35,
+    phone: "+62 812 242424",
+    email: "john@doe.com",
+    organization: "Impact Byte",
+    tags: "creative"
+  },
+  {
+    id: 3,
+    name: "Joe Smith",
+    age: 50,
+    phone: "+63 813 363636",
+    email: "joe@smith.dev",
+    organization: "Smith Corporation.",
+    tags: "smart"
+  }
+];
+
+// Show contact
+const showContacts = contacts => {
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+    console.log(
+      `[${contact.id}] ${contact.name} (${contact.phone}) <${contact.email}> ${
+        contact.organization
+      }`
+    );
+  }
+};
+
+// Call contact
+showContacts(myContacts)
+console.log("");
+
+// Filter contact based on organization name
+const filterContacts = (myContacts, similarOrganization) => {
+  let newContacts = [];
+
+  for (let index = 0; index < myContacts.length; index++) {
+    const contact = myContacts[index];
+
+    if (contact.organization.length <= similarOrganization) {
+      newContacts.push(contact);
+    }
+  }
+
+  return newContacts;
+};
+
+// Show Filter Contacts
+const filteredContacts = filterContacts(myContacts, 11);
+
+showContacts(filteredContacts)
+console.log("");
+
+// Add data into object
+const addData = () => {
+  for (let index = 0; index < myContacts.length; index++) {
+    myContacts[index].country = "Indonesia";
+  }
+  console.log(myContacts);
+};
+
+addData();
